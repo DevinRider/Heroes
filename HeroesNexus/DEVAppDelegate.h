7303679246fac99bf239e0aa4db784717b1ffe16
Devin Rider <devinrrider@gmail.com>
2014-06-15 13:59:48 -0700

    Initial Commit

---
+++ HeroesNexus/DEVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEVAppDelegate.h
+//  HeroesNexus
+//
+//  Created by Devin on 6/15/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HeroesNexus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HeroesNexus
+//
+//  Created by Devin on 6/15/14.
+//  Copyright (c) 2014 Devin Rider. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEVAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEVAppDelegate class]));
+    }
+}
